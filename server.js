39fc83db2c38f0597c6e552478d2a0cf2b5c7707
Evgeniy Kiryanov <evgeniy.kiryanov@roonyx.team>
2019-09-15 15:53:52 +0300

    added server

---
+++ server.js
@@ -0,0 +1,65 @@
+var http = require("http");
+var path = require("path");
+var fs = require("fs");
+var checkMimeType = true;
+
+http.createServer( function(req, res) {
+
+    var now = new Date();
+
+    var filename = req.url;
+    if (filename === '/') filename = "/index.html";
+    var ext = path.extname(filename);
+    var localPath = __dirname;
+    var validExtensions = {
+        ".html" : "text/html",
+        ".js": "application/javascript",
+        ".css": "text/css",
+        ".txt": "text/plain",
+        ".jpg": "image/jpeg",
+        ".gif": "image/gif",
+        ".png": "image/png",
+        ".woff": "application/font-woff",
+        ".woff2": "application/font-woff2"
+    };
+
+    var validMimeType = true;
+    var mimeType = validExtensions[ext];
+    if (checkMimeType) {
+        validMimeType = validExtensions[ext] != undefined;
+    }
+
+    if (validMimeType) {
+        localPath += filename;
+        fs.exists(localPath, function(exists) {
+            if(exists) {
+                console.log("Serving file: " + localPath);
+                getFile(localPath, res, mimeType);
+            } else {
+                console.log("File not found: " + localPath);
+                res.writeHead(404);
+                res.end();
+            }
+        });
+
+    } else {
+        console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
+    }
+
+}).listen(process.env.PORT || 3000);
+
+function getFile(localPath, res, mimeType) {
+    fs.readFile(localPath, function(err, contents) {
+        if(!err) {
+            res.setHeader("Content-Length", contents.length);
+            if (mimeType != undefined) {
+                res.setHeader("Content-Type", mimeType);
+            }
+            res.statusCode = 200;
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
+    });
+}
